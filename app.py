0e1d551d172c9d35586bc921f2db092cc452bc40
嗷嗚 <148690928+miaouwub@users.noreply.github.com>
2025-04-23 12:00:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ with st.sidebar:
 #圖表
 if choice=='圖表':
     st.write('# 桃園市各廟宇主祀神祇比例')
+    st.write('')
     st.image('https://i.ibb.co/F4bh2Jbx/Figure-1.png')
     st.image('https://pbs.twimg.com/media/GfI_wgIaoAANz64.jpg')
     st.image('https://pbs.twimg.com/media/GfI_wgIa4AA_OaW.jpg')
